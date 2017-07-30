f426840d51c2a68528a162a3a86323c132e2ecc5
Alexander Bragin <alexander.bragin@gmail.com>
2017-07-30 23:24:09 +0300

    Added static Void default value.

--- modules/RoxieMobile.CSharpCommons/src/RoxieMobile.CSharpCommons/Lang/Void.cs
+++ modules/RoxieMobile.CSharpCommons/src/RoxieMobile.CSharpCommons/Lang/Void.cs
@@ -1,14 +1,19 @@
 namespace RoxieMobile.CSharpCommons.Lang
 {
-    /**
-     * The {@code Void} class is an uninstantiable placeholder class to hold a
-     * reference to the {@code Class} object representing the C# keyword void.
-     */
+    /// <summary>
+    /// The {@code Void} class is an uninstantiable placeholder class to hold a reference
+    /// to the {@code Class} object representing the C# keyword void.
+    /// </summary>
     public sealed class Void
     {
-        /*
-         * The Void class cannot be instantiated.
-         */
+        /// <summary>
+        /// The Void class cannot be instantiated.
+        /// </summary>
         private Void() {}
+
+        /// <summary>
+        /// Gets the single unit value.
+        /// </summary>
+        public static Void Default { get; } = new Void();
     }
 }
