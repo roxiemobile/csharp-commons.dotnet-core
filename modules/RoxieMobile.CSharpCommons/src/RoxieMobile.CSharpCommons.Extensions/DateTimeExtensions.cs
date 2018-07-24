ea5cfb840066b2b7a3410438f31900eb5ea337d4
Alexander Bragin <BraginAV@ekassir.com>
2018-07-24 15:24:23 +0300

    Added static DateTime.ToUnixTimestamp() method.

---
+++ modules/RoxieMobile.CSharpCommons/src/RoxieMobile.CSharpCommons.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RoxieMobile.CSharpCommons.Extensions
+{
+    public static class DateTimeExtensions
+    {
+// MARK: - Methods
+
+        /// <summary>
+        /// Converts <c>DateTime</c> object to Unix timestamp.
+        /// </summary>
+        /// <param name="source"><c>DateTime</c> to convert.</param>
+        /// <returns>The Unix timestamp.</returns>
+        public static long ToUnixTimestamp(this DateTime source)
+        {
+            if (source == null) {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var totalSeconds = (source - UNIX_TIME_EPOCH).TotalSeconds;
+            return (long) Math.Floor(totalSeconds);
+        }
+
+// MARK: - Constants
+
+        private static readonly DateTime UNIX_TIME_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    }
+}
