c2439e0187c644c4d48ac6c39374e66f1cc97a01
Alexander Bragin <alexander.bragin@gmail.com>
2018-08-27 10:55:52 +0300

    Added class GenericActivator.

---
+++ modules/RoxieMobile.CSharpCommons/src/RoxieMobile.CSharpCommons.Data/Helpers/GenericActivator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RoxieMobile.CSharpCommons.Data.Helpers
+{
+    /// <summary>Contains methods to create types of objects.</summary>
+    public static class GenericActivator
+    {
+        /// <summary>Creates an instance of the type designated by the specified generic type parameter, using the constructor that best matches the specified parameters.</summary>
+        /// <typeparam name="T">The type of object to create.</typeparam>
+        /// <param name="args">An array of arguments that match in number, order, and type the parameters of the constructor to invoke. If args is an empty array or null, the constructor that takes no parameters (the default constructor) is invoked.</param>
+        /// <returns>A reference to the newly created object.</returns>
+        public static T NewInstance<T>(params object[] args)
+        {
+            return (T) Activator.CreateInstance(typeof(T), args);
+        }
+    }
+}
