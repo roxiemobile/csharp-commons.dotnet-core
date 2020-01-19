12e400f4dbe3b3b03620da02496850d089a36974
Alexander Bragin <alexander.bragin@gmail.com>
2020-01-19 15:29:22 +0300

    RMCMT-133 Migrated `Abstractions` project on the .NET Core 3.1 LTS.

--- Modules/RoxieMobile.CSharpCommons/src/Abstractions/Validators/IValidator.cs
+++ Modules/RoxieMobile.CSharpCommons/src/Abstractions/Validators/IValidator.cs
@@ -6,6 +6,6 @@ namespace RoxieMobile.CSharpCommons.Abstractions.Validators
         /// Tests the current state of the object.
         /// </summary>
         /// <returns><c>true</c> if object validation succeeded; otherwise, <c>false</c>.</returns>
-        bool IsValid(object value);
+        bool IsValid(object? value);
     }
 }
