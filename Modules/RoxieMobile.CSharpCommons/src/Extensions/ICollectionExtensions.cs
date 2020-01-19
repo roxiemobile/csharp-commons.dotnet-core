553b32f78767218fcfe0ee128120f753270ecb65
Alexander Bragin <alexander.bragin@gmail.com>
2020-01-19 15:38:04 +0300

    RMCMT-133 Migrated `Extensions` project on the .NET Core 3.1 LTS.

--- Modules/RoxieMobile.CSharpCommons/src/Extensions/ICollectionExtensions.cs
+++ Modules/RoxieMobile.CSharpCommons/src/Extensions/ICollectionExtensions.cs
@@ -1,32 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace RoxieMobile.CSharpCommons.Extensions
 {
-    [SuppressMessage("ReSharper", "ArrangeRedundantParentheses")]
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public static class ICollectionExtensions
     {
 // MARK: - Methods: Generic Collection
-
-        /// <summary>
-        /// Checks that a collection is <c>null</c> or empty. 
-        /// </summary>
-        /// <typeparam name="T">The type of the parameter.</typeparam>
-        /// <param name="collection">The collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is <c>null</c> or empty.</returns>
-        public static bool IsEmpty<T>(this ICollection<T> collection) =>
-            (collection == null) || (collection.Count < 1);
-
-        /// <summary>
-        /// Checks that a collection is not <c>null</c> and not empty. 
-        /// </summary>
-        /// <typeparam name="T">The type of the parameter.</typeparam>
-        /// <param name="collection">The collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is not <c>null</c> and not empty.</returns>
-        public static bool IsNotEmpty<T>(this ICollection<T> collection) =>
-            !collection.IsEmpty();
 
         /// <summary>
         /// Casts a specified collection to an <see cref="ICollection{T}"/> interface.
@@ -36,23 +16,5 @@ namespace RoxieMobile.CSharpCommons.Extensions
         /// <returns>A collection casted to an <see cref="ICollection{T}"/> interface.</returns>
         public static ICollection<T> AsCollection<T>(this ICollection<T> collection) =>
             collection;
-
-// MARK: - Methods: Collection
-
-        /// <summary>
-        /// Checks that a collection is <c>null</c> or empty. 
-        /// </summary>
-        /// <param name="collection">The collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is <c>null</c> or empty.</returns>
-        public static bool IsEmpty(this ICollection collection) =>
-            (collection == null) || (collection.Count < 1);
-
-        /// <summary>
-        /// Checks that a collection is not <c>null</c> and not empty. 
-        /// </summary>
-        /// <param name="collection">The collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is not <c>null</c> and not empty.</returns>
-        public static bool IsNotEmpty(this ICollection collection) =>
-            !collection.IsEmpty();
     }
 }
--- Modules/RoxieMobile.CSharpCommons/src/Extensions/IReadOnlyCollectionExtension.cs
+++ Modules/RoxieMobile.CSharpCommons/src/Extensions/IReadOnlyCollectionExtension.cs
@@ -3,29 +3,10 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace RoxieMobile.CSharpCommons.Extensions
 {
-    [SuppressMessage("ReSharper", "ArrangeRedundantParentheses")]
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     public static class IReadOnlyCollectionExtensions
     {
 // MARK: - Methods: Read-Only Generic Collection
-
-        /// <summary>
-        /// Checks that a read-only collection is <c>null</c> or empty. 
-        /// </summary>
-        /// <typeparam name="T">The type of the parameter.</typeparam>
-        /// <param name="collection">The read-only collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is <c>null</c> or empty.</returns>
-        public static bool IsEmpty<T>(this IReadOnlyCollection<T> collection) =>
-            (collection == null) || (collection.Count < 1);
-
-        /// <summary>
-        /// Checks that a read-only collection is not <c>null</c> and not empty. 
-        /// </summary>
-        /// <typeparam name="T">The type of the parameter.</typeparam>
-        /// <param name="collection">The read-only collection to check or <c>null</c>.</param>
-        /// <returns><c>true</c> if the <paramref name="collection"/> is not <c>null</c> and not empty.</returns>
-        public static bool IsNotEmpty<T>(this IReadOnlyCollection<T> collection) =>
-            !collection.IsEmpty();
 
         /// <summary>
         /// Casts a specified collection to an <see cref="IReadOnlyCollection{T}"/> interface.
